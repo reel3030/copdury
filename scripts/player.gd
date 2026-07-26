d9bc99c4d7d6952fdfc88ff70ce4615cb366babb
reel3030 <rikip5630@gmail.com>
2026-07-27 08:23:48 +0900

    add move script

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,6 @@
-extends Node
+extends CharacterBody2D
 
-
+@export var speed: float = 250.0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -9,3 +9,15 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _physics_process(_delta):
+	var direction = Input.get_vector(
+		"move_left",
+		"move_right",
+		"move_up",
+		"move_down"
+	)
+	velocity = direction * speed
+	move_and_slide()
+	
+	print(position)
