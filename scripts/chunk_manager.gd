5f68b851d0bbfdf28f7451a11635c4226914661b
reel3030 <rikip5630@gmail.com>
2026-07-28 10:32:16 +0900

    move scens and write eroor code

--- scripts/chunk_manager.gd
+++ scripts/chunk_manager.gd
@@ -4,7 +4,11 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-	print("ChankManager ready")
+	print("[ChankManager] ready")
+	var sources := int=ground.tile_set.get_source_count()
+	print("Source Count:", sources)
+	for i in range(sources):
+		print("SourceID:", ground.tile_set.get_source_id(i))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
