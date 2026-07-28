4d6dd3432850b243fc0119449055e3f248d5c8f7
reel3030 <rikip5630@gmail.com>
2026-07-28 10:42:22 +0900

    hmm.. camera not follow player

--- scripts/chunk_manager.gd
+++ scripts/chunk_manager.gd
@@ -5,10 +5,11 @@ extends Node
 func _ready() -> void:
 	pass # Replace with function body.
 	print("[ChankManager] ready")
-	var sources := int=ground.tile_set.get_source_count()
-	print("Source Count:", sources)
-	for i in range(sources):
+	var source_count: int = ground.tile_set.get_source_count()
+	print("Source Count:", source_count)
+	for i in range(source_count):
 		print("SourceID:", ground.tile_set.get_source_id(i))
+	ground.set_cell(Vector2i(0, 0), 0, Vector2i(0, 0))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
