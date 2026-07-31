12cf7f9a7d75bc9d44de056e4f729e35cafd3fd9
reel3030 <rikip5630@gmail.com>
2026-07-31 12:03:01 +0900

    add func

--- scripts/chunk_manager.gd
+++ scripts/chunk_manager.gd
@@ -5,12 +5,16 @@ extends Node
 func _ready() -> void:
 	pass # Replace with function body.
 	print("[ChankManager] ready")
+	
 	var source_count: int = ground.tile_set.get_source_count()
 	print("Source Count:", source_count)
 	for i in range(source_count):
 		print("SourceID:", ground.tile_set.get_source_id(i))
-	ground.set_cell(Vector2i(0, 0), 0, Vector2i(0, 078o))
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	generate_test_map()
+	
+	func generate_test_map() -> void:
+		ground.set_cell(
+			Vector2i(0,0),
+			0,
+			Vector2i(0,0)
+		)
