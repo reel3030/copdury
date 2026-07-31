984f9e44587864a91499e2c9568bf148922523e7
reel3030 <rikip5630@gmail.com>
2026-07-31 11:50:56 +0900

    edit code -1

--- scripts/chunk_manager.gd
+++ scripts/chunk_manager.gd
@@ -1,6 +1,6 @@
 extends Node
 
-@onready var ground = $"../Ground"
+@onready var ground: TileMapLayer = $"../Ground"
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -9,7 +9,7 @@ func _ready() -> void:
 	print("Source Count:", source_count)
 	for i in range(source_count):
 		print("SourceID:", ground.tile_set.get_source_id(i))
-	ground.set_cell(Vector2i(0, 0), 0, Vector2i(0, 0))
+	ground.set_cell(Vector2i(0, 0), 0, Vector2i(0, 078o))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
