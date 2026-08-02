86d2398bc948df30e59814d5cec5ab7c1ec46d3f
りーる <rikip5630@gmail.com>
2026-08-02 19:04:39 +0900

    fix player
    fix player speed,and player size

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@export var speed: float = 250.0
+@export var speed: float = 100
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
